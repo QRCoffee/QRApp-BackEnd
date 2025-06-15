350440d2dca3000323da60d34e64426bb30f2f23
nhathuyd4hp <nhathuyd4hp@gmail.com>
2025-06-15 13:08:30 +0700

    Merge middleware

--- app/core/middleware/logging.py
+++ app/core/middleware/logging.py
@@ -1,29 +1,54 @@
 import time
-
+from typing import Dict,Any
 from fastapi import Request
 from loguru import logger
 from starlette.middleware.base import BaseHTTPMiddleware
-
+from app.common.enum import APIError, APIMessage
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import ResponseValidationError
 
 class LoggingMiddleware(BaseHTTPMiddleware):
+    def _get_request_info(self, request: Request) -> Dict[str, Any]:
+        """Extract common request information"""
+        return {
+            "host": request.client.host,
+            "user_agent": request.headers.get("user-agent", "unknown"),
+            "method": request.method,
+            "path": request.url.path,
+        }
     async def dispatch(self, request: Request, call_next):
         start_time = time.time()
-        response = await call_next(request)
-        duration = time.time() - start_time
-        # ---- Extract information of request
-        method = request.method
-        path = request.url.path
-        host = request.client.host
-        user_agent = request.headers.get("user-agent","unknown")
-        # ---- Extract information of response
-        status_code = response.status_code
-        logger.info({
-            "host": host,
-            "user-agent":user_agent,
-            "method": method,
-            "path": path,
-            "duration": duration,
-            "status_code": status_code,
-            "error": None,
-        })
-        return response
+        try:
+            response = await call_next(request)
+            duration = time.time() - start_time
+            log_data = {
+                **self._get_request_info(request),
+                "duration": duration,
+                "status_code": response.status_code,
+                "error": None
+            }
+            logger.info(log_data)
+            return response
+        except Exception as e:
+            duration = time.time() - start_time
+            status_code = 500
+            error = APIError.SERVER_ERROR
+            message = APIMessage.SERVER_ERROR
+            if isinstance(e,ResponseValidationError):
+                status_code = 422
+                error = APIError.VALIDATION_ERROR
+                message = [f"{error['msg']} {error['loc']}" for error in e.errors()]
+            log_data = {
+                **self._get_request_info(request),
+                "duration": duration,
+                "status_code": status_code,
+                "error": type(e).__name__
+            }
+            logger.error(log_data)
+            return JSONResponse(
+                status_code=status_code,
+                content={
+                    "error": error,
+                    "message": message
+                }
+            )
