a22906c89c3c5aa99ed2bdd25323017d01dc5427
Van Ngoc Nhat Huy <nhathuyd4hp@gmail.com>
2025-06-16 08:08:04 +0700

    __init__.py

--- app/schema/__init__.py
+++ app/schema/__init__.py
@@ -8,7 +8,7 @@ from pydantic import BaseModel, ConfigDict, Field, field_validator
 class BaseResponse(BaseModel):
     model_config = ConfigDict(
         populate_by_name= True,
-        from_attributes=True,
+        from_attributes = True,
     )
     id: Optional[str] = Field(alias="_id")
     created_at: Optional[datetime]
